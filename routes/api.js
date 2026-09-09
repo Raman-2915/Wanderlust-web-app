3ddf7d143213f88b576b6f4f1d7e0e864fa76005
Raman-2915 <rdeep2903@gmail.com>
2026-09-10 00:27:55 +0530

    feat: add protected booking API routes

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const apiController = require("../controllers/api.js");
+const { isLoggedIn } = require("../middleware.js");
+const apiRateLimit = require("../middleware/apiRateLimit.js");
+
+router.use(apiRateLimit);
 
 router.get("/health", apiController.health);
 router.get("/listings", wrapAsync(apiController.listListings));
 router.get("/listings/:id", wrapAsync(apiController.getListing));
+router.post("/listings/:id/bookings", isLoggedIn, wrapAsync(apiController.createBooking));
+router.delete("/bookings/:bookingId", isLoggedIn, wrapAsync(apiController.cancelBooking));
 
 module.exports = router;
