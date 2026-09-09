3667caa08e437ffd6911523f82a1e6ccb262b75a
Raman-2915 <rdeep2903@gmail.com>
2026-09-10 00:28:16 +0530

    feat: add JSON authentication middleware

---
+++ middleware/apiAuth.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({
+      success: false,
+      message: "Authentication required",
+    });
+  }
+  next();
+};
