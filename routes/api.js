9912b3ade51b36bc7a86b00408458b860cac61c2
Raman-2915 <rdeep2903@gmail.com>
2026-09-11 01:25:28 +0530

    Verify CSRF tokens on mutating API requests

--- routes/api.js
+++ routes/api.js
@@ -4,13 +4,14 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const apiController = require("../controllers/api.js");
 const apiAuth = require("../middleware/apiAuth.js");
 const apiRateLimit = require("../middleware/apiRateLimit.js");
+const csrf = require("../middleware/csrf.js");
 
 router.use(apiRateLimit);
 
 router.get("/health", apiController.health);
 router.get("/listings", wrapAsync(apiController.listListings));
 router.get("/listings/:id", wrapAsync(apiController.getListing));
-router.post("/listings/:id/bookings", apiAuth, wrapAsync(apiController.createBooking));
-router.delete("/bookings/:bookingId", apiAuth, wrapAsync(apiController.cancelBooking));
+router.post("/listings/:id/bookings", apiAuth, csrf.protect, wrapAsync(apiController.createBooking));
+router.delete("/bookings/:bookingId", apiAuth, csrf.protect, wrapAsync(apiController.cancelBooking));
 
 module.exports = router;
