da291640718c9d6aa70d3af827e93ebf964d4591
Raman-2915 <rdeep2903@gmail.com>
2026-09-11 01:24:49 +0530

    Add baseline security headers

---
+++ middleware/securityHeaders.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+  res.set({
+    "X-Content-Type-Options": "nosniff",
+    "X-Frame-Options": "DENY",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
+    "Permissions-Policy": "camera=(), microphone=(), geolocation=()",
+  });
+  next();
+};
