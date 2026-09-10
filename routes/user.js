3c2340359c994a12e358d5cf356ee8971e0f4d19
Raman-2915 <rdeep2903@gmail.com>
2026-09-11 01:25:30 +0530

    Protect authentication endpoints and require POST logout

--- routes/user.js
+++ routes/user.js
@@ -4,16 +4,20 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { isLoggedIn, saveRedirectUrl } = require("../middleware");
 const userController = require("../controllers/user");
+const { createAuthRateLimit } = require("../middleware/authRateLimit.js");
+
+const authRateLimit = createAuthRateLimit();
 
 router
   .route("/signup")
   .get(userController.renderSignUpForm)
-  .post(wrapAsync(userController.userSignUp));
+  .post(authRateLimit, wrapAsync(userController.userSignUp));
 
 router
   .route("/login")
   .get(userController.renderLoginForm)
   .post(
+    authRateLimit,
     saveRedirectUrl,
     passport.authenticate("local", {
       failureRedirect: "/login",
@@ -23,6 +27,6 @@ router
   );
 
 router.get("/profile", isLoggedIn, wrapAsync(userController.renderDashboard));
-router.get("/logout", userController.userLogout);
+router.post("/logout", userController.userLogout);
 
 module.exports = router;
