f315606a6d701b79ba7b629efee11c6209f4937d
Raman-2915 <rdeep2903@gmail.com>
2026-09-16 23:47:20 +0530

    Add login and signup rate limiting

--- routes/user.js
+++ routes/user.js
@@ -3,17 +3,19 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { isLoggedIn, saveRedirectUrl } = require("../middleware");
+const { authRateLimit } = require("../middleware/security.js");
 const userController = require("../controllers/user");
 
 router
   .route("/signup")
   .get(userController.renderSignUpForm)
-  .post(wrapAsync(userController.userSignUp));
+  .post(authRateLimit, wrapAsync(userController.userSignUp));
 
 router
   .route("/login")
   .get(userController.renderLoginForm)
   .post(
+    authRateLimit,
     saveRedirectUrl,
     passport.authenticate("local", {
       failureRedirect: "/login",
